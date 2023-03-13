5109e56ed03a9b3ed0cfb788e4b61f9a16b6d0d9
Ivan Cherviakovskiy <ivancherviakovskiy@gmail.com>
2023-03-13 16:36:18 +0300

    Фdded logic for sending an order request

--- src/components/Cart/SubmitOrder.js
+++ src/components/Cart/SubmitOrder.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import styles from './SubmitOrder.module.css';
 
 const SubmitOrder = (props) => {
+    const cartContext = useContext(CartContext);
+    const fetchCartDelivery = async () => {
+        const response = await fetch('https://react-joke-course-default-rtdb.europe-west1.firebasedatabase.app/delivery-meals.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ items: cartContext.items, totalAmount: cartContext.totalAmount }),
+        })
+        if (!response.ok) {
+            throw new Error('Ошибка в обработке заказа');
+        }
+        const data = await response.json();
+        console.log(data);
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        try {
+            if (cartContext.items.length === 0 && cartContext.totalAmount === 0) {
+                throw new Error('В корзине нет оваров для заказа');
+            }
+            fetchCartDelivery();
+        }
+        catch (e) {
+            console.log(e.message);
+        }
+    }
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={submitHandler}>
             <div className={styles.control}>
                 <label htmlFor='name'>Имя</label>
                 <input id='name' type='text' />
